c1b53dcfda3ca6277ea36e7d7490c7f5e8153272
sy-records <52o@qq52o.cn>
2019-09-12 17:03:13 +0800

    单例模式

---
+++ Singleton/Singleton.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * User: lufei
+ * Date: 2019/9/12
+ * Email: lufei@swoole.com
+ */
+
+namespace Luffy\DesignPatterns\Singleton;
+
+/**
+ * Class Singleton
+ * @package Luffy\DesignPatterns\Singleton
+ */
+class Singleton
+{
+	/**
+	 * @var
+	 */
+	private static $instance;
+
+	public static function getInstance()
+	{
+		if (!self::$instance instanceof self) {
+			static::$instance = new self();
+		}
+
+		return static::$instance;
+	}
+
+	/**
+	 * MySQLDB constructor.
+	 */
+	private function __construct()
+	{
+
+	}
+
+	private function __clone()
+	{
+
+	}
+
+	private function __wakeup()
+	{
+		// TODO: Implement __wakeup() method.
+	}
+}
