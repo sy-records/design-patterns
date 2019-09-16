738884c5f99d53c1d9f347b3f8a52b2dac486d60
sy-records <52o@qq52o.cn>
2019-09-16 08:51:16 +0800

    客户端测试文件

---
+++ Factory/FactoryMethod/Client.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: lufei
+ * Date: 2019/9/16
+ * Email: lufei@swoole.com
+ */
+
+include __DIR__ . '/../../vendor/autoload.php';
+
+use Luffy\DesignPatterns\Factory\SimpleFactory\FactoryAli;
+use Luffy\DesignPatterns\Factory\SimpleFactory\FactoryWeChat;
+
+/**
+ * 具体生产对象并执行对象方法测试
+ */
+class Client
+{
+	public function index()
+	{
+		$factory = new FactoryAli();
+		$ali = $factory->create();
+		$ali->pay();
+
+
+		$factory = new FactoryWeChat();
+		$ali = $factory->create();
+		$ali->pay();
+	}
+}
+
+// 执行
+$demo = new Client;
+$demo->index();
