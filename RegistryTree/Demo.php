25d0f23e04be0fec6dcc4baec509c2536634e098
sy-records <52o@qq52o.cn>
2019-10-15 19:58:15 +0800

    :art: update code

--- RegistryTree/Demo.php
+++ RegistryTree/Demo.php
@@ -25,11 +25,17 @@ $demo = new Demo();
 var_dump($demo);
 $demo->index();
 
+// 添加进对象树中
 RegistryTree::set("demo", $demo);
+
+// 获取对象
+$demo2 = RegistryTree::get("demo");
+var_dump($demo2);
+$demo2->index();
+
+// 删除对象
+RegistryTree::_unset("demo");
+// 再次获取
 $demo_obj = RegistryTree::get("demo");
 var_dump($demo_obj);
 $demo_obj->index();
-
-RegistryTree::_unset('demo');
-$demo_obj_two = RegistryTree::get('demo');
-$demo_obj_two->index();
